72248a2cda0d710b6631d387b7256b3dc9a77345
Noodle1991 <staspesetsky@gmail.com>
2022-12-16 18:34:46 +0300

    added smth

---
+++ bmstu_stroka.h
@@ -0,0 +1,201 @@
+#pragma once
+
+#include <iostream>
+
+namespace bmstu {
+    template<typename T>
+    class basic_string;
+
+    using string = basic_string<char>;
+    using wstring = basic_string<wchar_t>;
+
+    template<typename T>
+    class basic_string {
+    public:
+
+///конструктор по умолчанию
+        basic_string() {
+            ptr_ = new T[1];
+            *ptr_ = '\0';
+            size_ = 0;
+        }
+
+/// Конструктор с параметром "cи строкой"
+        basic_string(const T *c_str) {
+            clean_();
+            size_ = strlen_(c_str);
+            ptr_ = static_cast<T *>(new T[size_ + 1]);
+            ptr_[size_] = 0;
+            for (int c = 0; c < size_; ++c) {
+                ptr_[c] = c_str[c];
+            }
+        }
+
+/// Копирующий конструктор
+        basic_string(const basic_string<T> &other) {
+            clean_();
+            size_ = other.size_;
+            ptr_ = new T[size_ + 1];
+            ptr_[size_] = 0;
+            for (int i = 0; i < size_; i++) {
+                *(ptr_ + i) = other.c_str()[i];
+            }
+        }
+
+/// Конструктор перемещения
+        basic_string(basic_string<T> &&dying) noexcept {
+            if (this != &dying) {
+                clean_();
+                ptr_ = dying.ptr_;
+                size_ = dying.size_;
+                dying.ptr_ = new T[1];
+                dying.size_ = 0;
+            }
+        }
+
+/// Деструктор
+        ~basic_string() {
+            delete[] ptr_;
+        }
+
+/// Геттер на си-строку
+        const T *c_str() const {
+            return static_cast<const T* >(ptr_);
+        }
+
+/// Геттер на размер
+        size_t size() const {
+            return size_;
+        }
+
+/// Оператор копирующего присваивания
+        basic_string &operator=(const basic_string<T> &other) {
+            if (this != &other) {
+                clean_();
+                size_ = other.size_;
+                ptr_ = new T[size_ + 1];
+                ptr_[size_] = 0;
+                for (int i = 0; i < size_; i++) {
+                    *(ptr_ + i) = other.c_str()[i];
+                }
+            }
+            return *this;
+        }
+
+/// Оператор перемещающего присваивания
+        basic_string &operator=(basic_string<T> &&other) noexcept {
+            if (this != &other) {
+                clean_();
+                ptr_ = other.ptr_;
+                size_ = other.size_;
+                other.ptr_ = new T[1];
+                other.size_ = 0;
+            }
+            return *this;
+        }
+
+/// Оператор присваивания си строки
+        basic_string &operator=(const T *c_str) {
+            clean_();
+            size_ = strlen_(c_str);
+            ptr_ = new T[size_ + 1];
+            ptr_[size_] = 0;
+            for (int i = 0; i < size_; i++) {
+                ptr_[i] = c_str[i];
+            }
+            return *this;
+        }
+
+/*Все все понимают*/
+
+        friend bmstu::basic_string<T> operator+(const basic_string<T> &left, const basic_string<T> &right) {
+            bmstu::basic_string<T> result(left);
+            result += right;
+            return result;
+        }
+
+        template<class OS> ///типа std::ostream
+        /* скорее так правильно что делать для других потоков*/
+        friend OS &operator<<(OS &os, const basic_string<T> &obj) {
+            os << obj.c_str();
+            return os;
+        }
+
+        template<class IS>///типа std::istream
+        friend IS &operator>>(IS &is, basic_string<T> &obj) {
+            basic_string result;
+            T buf = ' ';
+            while (is.good()) {
+                buf = is.get();
+                if (buf == -1) {
+                    break;
+                }
+                result += buf;
+            }
+            obj = std::move(result);
+            return is;
+        }
+
+        basic_string &operator+=(const basic_string<T> &other) {
+            size_t new_size = size_ + other.size_;
+            auto new_ptr = new T[new_size + 1];
+            for (int i = 0; i < size_; i++) {
+                new_ptr[i] = ptr_[i];
+            }
+
+            for (int i = 0; i < other.size_; i++) {
+                new_ptr[i + size_] = other.ptr_[i];
+            }
+
+            new_ptr[new_size] = 0;
+            clean_();
+            ptr_ = new_ptr;
+            size_ = new_size;
+            return *this;
+        }
+
+        basic_string &operator+=(T symbol) {
+            auto new_size = size_ + 1;
+            auto new_ptr = new T[new_size + 1];
+            for (auto i = 0; i < size_; ++i) {
+                new_ptr[i] = ptr_[i];
+            }
+            new_ptr[new_size - 1] = symbol;
+            new_ptr[new_size] = 0;
+            clean_();
+            ptr_ = new_ptr;
+            size_ = new_size;
+            return *this;
+        }
+
+        T &operator[](size_t index) {
+            if (index < size_) {
+                return *(ptr_ + index);
+            }
+            throw std::runtime_error("No index" + std::to_string(index));
+        }
+
+    private:
+/// приватные хелперы
+        static size_t strlen_(const T *str) {
+            size_t result = 0;
+            while (*str) {
+                result = result + 1;
+                str = str + 1;
+            }
+            return result;
+        }
+
+        void clean_() {
+            if (ptr_ != nullptr) {
+                delete[] ptr_;
+                ptr_ = nullptr;
+            }
+            size_ = 0;
+        }
+
+/// Поля класса
+        T *ptr_ = nullptr;
+        int size_;
+    };
+}
--- example_test.cpp
+++ example_test.cpp
@@ -1,10 +1,264 @@
 #include <gtest/gtest.h>
-#include <string>
+#include "bmstu_stroka.h"
+
 TEST(GoogleTestCi, Foo) {
-  ASSERT_TRUE(true);
+    ASSERT_TRUE(true);
 }
-TEST(ExampleTest, DmitriiLeonidovich){
+
+TEST(ExampleTest, DmitriiLeonidovich) {
     std::string dml = "DmitriiLeonidovich";
-    const char * dml_c = "DmitriiLeonidovich";
+    const char *dml_c = "DmitriiLeonidovich";
     ASSERT_STREQ(dml.c_str(), dml_c);
+}
+
+TEST(Empty, empty_raw) {
+    bmstu::string stroka("");
+    ASSERT_STREQ(stroka.c_str(), "");
+}
+
+TEST(Сonstructor, raw) {
+    bmstu::string stroka("Boost");
+    ASSERT_STREQ(stroka.c_str(), "Boost");
+}
+
+TEST(Сonstructor, Constr_cstrrus) {
+    bmstu::wstring stroka(L"дота");
+    ASSERT_STREQ(stroka.c_str(), L"дота");
+}
+
+
+TEST(Copy, copy_raw) {
+    bmstu::string const stroka("Boost");
+    bmstu::string stroka2(stroka);
+    ASSERT_STREQ(stroka2.c_str(), "Boost");
+}
+
+TEST(Copy, copy_empty_raw) {
+    bmstu::string stroka("");
+    bmstu::string stroka2(stroka);
+    ASSERT_STREQ(stroka2.c_str(), "");
+}
+
+TEST(Moving, move_raw) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2(std::move(stroka));
+    ASSERT_STREQ(stroka2.c_str(), "Boost");
+}
+
+TEST(Moving, move_empty_raw) {
+    bmstu::string stroka("");
+    bmstu::string stroka2(std::move(stroka));
+    ASSERT_STREQ(stroka2.c_str(), "");
+}
+
+TEST(Size, raws_size) {
+    bmstu::string stroka("Boost");
+    ASSERT_EQ(stroka.size(), 5);
+}
+
+TEST(Size, raws_size_2bytes) {
+    bmstu::wstring stroka(L"дота");
+    ASSERT_EQ(stroka.size(), 4);
+}
+
+TEST(Size, raws_size_nmbrs) {
+    bmstu::wstring stroka(L"123456");
+    ASSERT_EQ(stroka.size(), 6);
+}
+
+TEST(Size, raws_size_2bytes_and_nmbrs) {
+    bmstu::wstring stroka(L"дота2");
+    ASSERT_EQ(stroka.size(), 5);
+}
+
+TEST(Getter, getter_empty) {
+    bmstu::string stroka("");
+    ASSERT_EQ(stroka.size(), 0);
+
+}
+
+TEST(Copy, operator_copy) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2("Air");
+    stroka = stroka2;
+    ASSERT_STREQ(stroka.c_str(), "Air");
+
+}
+
+TEST(Copy, from_full_to_empty) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2("");
+    stroka = stroka2;
+    ASSERT_STREQ(stroka.c_str(), "");
+}
+
+TEST(Copy, from_empty_to_full) {
+    bmstu::string stroka("");
+    bmstu::string stroka2("Air");
+    stroka = stroka2;
+    ASSERT_STREQ(stroka.c_str(), "Air");
+}
+
+TEST(Copy, operator_copy_in) {
+    bmstu::string stroka("Boost");
+    stroka = stroka;
+    ASSERT_STREQ(stroka.c_str(), "Boost");
+}
+
+TEST(Replace, Operator_replace) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2("Air");
+    stroka = std::move(stroka2);
+    ASSERT_STREQ(stroka.c_str(), "Air");
+}
+
+TEST(Replace, replace_from_empty_to_full) {
+    bmstu::string stroka("");
+    bmstu::string stroka2("Air");
+    stroka = std::move(stroka2);
+    ASSERT_STREQ(stroka.c_str(), "Air");
+}
+
+TEST(Replace, replace_from_full_to_empty) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2("");
+    stroka = std::move(stroka2);
+    ASSERT_STREQ(stroka.c_str(), "");
+}
+
+TEST(Replace, repalace) {
+    bmstu::string stroka("Boost");
+    stroka = std::move(stroka);
+    ASSERT_STREQ(stroka.c_str(), "Boost");
+}
+
+TEST(Replace, replace_c) {
+    bmstu::string stroka("Boost");
+    stroka = "Air";
+    ASSERT_STREQ(stroka.c_str(), "Air");
+}
+
+TEST(Replace, replace_empty_c) {
+    bmstu::string stroka("");
+    stroka = "Air";
+    ASSERT_STREQ(stroka.c_str(), "Air");
+}
+
+TEST(Replace, replace_full_c) {
+    bmstu::string stroka("Boost");
+    stroka = "";
+    ASSERT_STREQ(stroka.c_str(), "");
+}
+
+TEST(Replace, put_smth_into_c) {
+    bmstu::string stroka;
+    stroka = "Boost";
+    ASSERT_STREQ(stroka.c_str(), "Boost");
+}
+
+TEST(Concatination, concatination) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2("Air");
+    stroka += stroka2;
+    ASSERT_STREQ(stroka.c_str(), "BoostAir");
+}
+
+TEST(Concatination, concatination_with_full) {
+    bmstu::string stroka("");
+    bmstu::string stroka2("Air");
+    stroka += stroka2;
+    ASSERT_STREQ(stroka.c_str(), "Air");
+}
+
+TEST(Concatination, concatination_with_full_2) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2("");
+    stroka += stroka2;
+    ASSERT_STREQ(stroka.c_str(), "Boost");
+}
+
+TEST(Concatination, concatination_with_full_3) {
+    bmstu::string stroka("");
+    bmstu::string stroka2("");
+    stroka += stroka2;
+    ASSERT_STREQ(stroka.c_str(), "");
+}
+
+TEST(Concatination, concatination_2bytes) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2("дота");
+    stroka += stroka2;
+    ASSERT_STREQ(stroka.c_str(), "Boostдота");
+}
+
+TEST(Concatination, concatination_with_1_symbol) {
+    bmstu::string stroka("Boost");
+    stroka += 'a';
+    ASSERT_STREQ(stroka.c_str(), "Boosta");
+}
+
+TEST(Concatination, concatination_with_null) {
+    bmstu::string stroka("Boost");
+    stroka += '\0';
+    ASSERT_STREQ(stroka.c_str(), "Boost");
+}
+
+TEST(Concatination, concatination_with_empty) {
+    bmstu::string stroka("");
+    stroka += '\0';
+    ASSERT_STREQ(stroka.c_str(), "");
+}
+
+TEST(Concatination, concatination_with_empty_2) {
+    bmstu::string stroka("");
+    stroka += 'a';
+    ASSERT_STREQ(stroka.c_str(), "a");
+}
+
+TEST(Concatination, different_concatination) {
+    bmstu::string stroka("Boost");
+    bmstu::string stroka2("Air");
+    bmstu::string super_stroka = stroka + stroka2;
+    ASSERT_STREQ(super_stroka.c_str(), "BoostAir");
+}
+
+TEST(Concatination, different_concatination_with_empty) {
+    bmstu::string stroka("");
+    bmstu::string stroka2("Air");
+    bmstu::string super_stroka = stroka + stroka2;
+    ASSERT_STREQ(super_stroka.c_str(), "Air");
+}
+
+TEST(Concatination, different_concatination_with_2bytes) {
+    bmstu::string stroka("дота");
+    bmstu::string stroka2("Minecraft");
+    bmstu::string super_stroka = stroka + stroka2;
+    ASSERT_STREQ(super_stroka.c_str(), "дотаMinecraft");
+}
+
+
+TEST(ExactSymbol, 1_symbol) {
+    bmstu::string stroka("Boost");
+    ASSERT_EQ(stroka.c_str()[0], 'B');
+}
+
+TEST(ExactSymbol, 4_symbol) {
+    bmstu::string stroka("Boost");
+    ASSERT_EQ(stroka.c_str()[3], 's');
+}
+
+TEST(Put, put) {
+    std::stringstream stroka;
+    stroka.str("BoostAir\n");
+    bmstu::string stroka2("Air");
+    stroka << stroka2;
+    ASSERT_STREQ(stroka.str().c_str(), "AirstAir\n");
+}
+
+TEST(Put, put_nmbrs) {
+    std::stringstream stroka;
+    stroka.str("Boost\n");
+    bmstu::string stroka2("123");
+    stroka << stroka2;
+    ASSERT_STREQ(stroka.str().c_str(), "123st\n");
 }
